fe07a2261ac21fc234887f68a6c82fc6a40ea3f3
federicofanini <federico.fanini9@gmail.com>
2025-01-04 13:16:16 +0100

    workout carousel

--- src/components/dashboard/dashboard-page.tsx
+++ src/components/dashboard/dashboard-page.tsx
@@ -1,13 +1,21 @@
 import { AchievementsList } from "./achivements-list";
 import { ProfileSummary } from "./profile-summary";
+import { WorkoutCarousel } from "./workout-carousel";
 import { WorkoutPlan } from "./workout-plan";
 
 export default function DashboardPage() {
   return (
-    <div className="flex gap-4">
-      <ProfileSummary />
-      <AchievementsList />
-      <WorkoutPlan />
-    </div>
+    <>
+      <div className="grid grid-cols-1 gap-4 md:flex">
+        <ProfileSummary />
+        <AchievementsList />
+        <WorkoutPlan />
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:flex">
+        <WorkoutCarousel />
+        <AchievementsList />
+        <WorkoutPlan />
+      </div>
+    </>
   );
 }
