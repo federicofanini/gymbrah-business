10479bdba125115ebe0c42aa3dbf18a2f15f2966
federicofanini <federico.fanini9@gmail.com>
2025-01-04 17:38:19 +0100

    og

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -29,64 +29,7 @@ export async function GET(req: NextRequest) {
           fontSize: 32,
           fontWeight: 600,
         }}
-      >
-        <div
-          style={{
-            position: "relative",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            top: "125px",
-          }}
-        >
-          <Icons.logo
-            style={{
-              color: "#fff",
-              width: "64px",
-              height: "64px",
-            }}
-          />
-
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              fontSize: "64px",
-              fontWeight: "600",
-              marginTop: "24px",
-              textAlign: "center",
-              color: "#fff",
-              width: "60%",
-              letterSpacing: "-0.05em", // Added tighter tracking
-            }}
-          >
-            {postTitle}
-          </div>
-          <div
-            style={{
-              display: "flex",
-              fontSize: "16px",
-              fontWeight: "500",
-              marginTop: "16px",
-              color: "#fff",
-            }}
-          >
-            {siteConfig.name}
-          </div>
-        </div>
-
-        <img
-          src={`${siteConfig.url}/cube.png`}
-          width={500}
-          style={{
-            position: "relative",
-            bottom: -100,
-            aspectRatio: "auto",
-          }}
-        />
-      </div>
+      ></div>
     ),
     {
       width: 1200,
