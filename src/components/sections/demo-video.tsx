51608fd8e0406faadcba17f723acc0cc40120242
federicofanini <federico.fanini9@gmail.com>
2025-01-12 20:09:43 +0100

    demo video

--- src/components/sections/demo-video.tsx
+++ src/components/sections/demo-video.tsx
@@ -8,14 +8,14 @@ export function DemoVideo() {
         <HeroVideoDialog
           className="dark:hidden block"
           animationStyle="from-center"
-          videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+          videoSrc="https://youtu.be/JMYqvPTIE2E"
           thumbnailSrc="/blackboard.png"
           thumbnailAlt="Hero Video"
         />
         <HeroVideoDialog
           className="hidden dark:block"
           animationStyle="from-center"
-          videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+          videoSrc="https://youtu.be/JMYqvPTIE2E"
           thumbnailSrc="/blackboard.png"
           thumbnailAlt="Hero Video"
         />
