7af74b1f1dbc252b904e0be5aac1256d76f8ffcb
federicofanini <federico.fanini9@gmail.com>
2025-01-12 13:01:25 +0100

    leaderboard revalidation

--- src/app/(private)/blackboard/leaderboard/page.tsx
+++ src/app/(private)/blackboard/leaderboard/page.tsx
@@ -1,5 +1,7 @@
 import { LeaderboardTable } from "@/components/blackboard/leaderboard/leaderboard-table";
 
+export const revalidate = 1800; // 30 minutes in seconds
+
 export default function LeaderboardPage() {
   return (
     <div>
