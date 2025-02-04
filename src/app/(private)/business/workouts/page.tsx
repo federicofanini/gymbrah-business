10ac84413557957a8fa9e1be11a4c58e869e2c76
federicofanini <federico.fanini9@gmail.com>
2025-02-04 16:57:06 +0100

    build fix

--- src/app/(private)/business/workouts/page.tsx
+++ src/app/(private)/business/workouts/page.tsx
@@ -1,14 +1,16 @@
 import { WorkoutPage } from "@/components/private/workout";
 import { getExercises } from "@/actions/exercises/exercises-list";
 
+interface SearchParams {
+  bodyPart?: string;
+  page?: string;
+  search?: string;
+}
+
 export default async function Workouts({
   searchParams,
 }: {
-  searchParams: {
-    bodyPart?: string;
-    page?: string;
-    search?: string;
-  };
+  searchParams: SearchParams & Promise<any>;
 }) {
   const selectedBodyPart = searchParams.bodyPart || "all";
   const currentPage = Number(searchParams.page) || 1;
