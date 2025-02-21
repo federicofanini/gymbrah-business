6785d9da26a38f0d05d79ad6aff4e1fa4394d3f7
federicofanini <federico.fanini9@gmail.com>
2025-02-21 17:55:41 +0100

    email link

--- src/components/private/email/welcome-athlete.tsx
+++ src/components/private/email/welcome-athlete.tsx
@@ -36,7 +36,7 @@ export async function sendWelcomeAthleteEmail({
 
           <div style="margin: 32px 0;">
             <a 
-              href="https://athlete.gymbrah.com/login?athlete_code=${athleteCode}" 
+              href="https://athlete.gymbrah.com?athlete_code=${athleteCode}" 
               style="background-color: #000000; color: white; padding: 12px 24px; text-decoration: none; border-radius: 6px; display: inline-block;"
             >
               Get Started
